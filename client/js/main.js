767a2402dd7f053f4822bde4ff6485fe98bc33ec
khalid <rabaaouikhalid@gmail.com>
2025-10-25 18:16:37 +0200

    add card

---
+++ client/js/main.js
@@ -0,0 +1,13 @@
+const img = document.getElementById("img");
+
+img.addEventListener("click", () => {
+    console.log("test");
+  img.classList.toggle("large");
+  
+});
+
+function namee() {
+    alert("hello");
+}
+
+namee();
