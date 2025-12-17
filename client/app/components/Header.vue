ab9432204026097e142c0a7baea21d9b1b95a472
Sherif <sdbdjjrjr@gmail.com>
2025-12-17 20:42:44 +0200

    addded useCookie to save the login state

--- client/app/components/Header.vue
+++ client/app/components/Header.vue
@@ -40,7 +40,7 @@ const auth = useAuthStore();
         <button
           v-if="auth.user"
           @click="auth.logout()"
-          class="px-3 py-1.5 rounded-lg font-medium text-white bg-vibrant-purple transition-colors duration-200 hover:bg-vibrant-purple/80"
+          class="px-3 py-1.5 rounded-lg font-medium text-white bg-vibrant-purple transition-colors duration-200 hover:bg-danger/80"
         >
           Logout
         </button>
